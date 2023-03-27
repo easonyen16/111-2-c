9053c0b8a6c02ff1b1ecb75497d5922b7166e83f
easonyen16 <eason990024@gmail.com>
2023-03-27 15:36:11 +0800

    Add files via upload

---
+++ 10-1.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+int gcd(int x, int y){
+    int t = x%y;
+    int loops = 1;
+    while(t!=0){
+        printf("Loop %d: %d / %d = %d ... %d\n", loops, x, y, x/y, t);
+        x = y;
+        y = t;
+        t = x%y;
+        loops++;
+    }
+    printf("Loop %d: %d / %d = %d ... %d\n", loops, x, y, x/y, t);
+    return y;
+}
+int main(){
+    int x, y;
+    int t;
+    scanf("%d", &t);
+    for(int i=0; i<t; i++){
+        printf("Case %d:\n", i+1);
+        scanf("%d %d", &x, &y);
+        printf("G.C.D. of %d and %d is %d\n", x, y, gcd(x, y));
+    }
+}
