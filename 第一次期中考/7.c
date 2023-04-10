9cefd715d412abdf6a77742a5340f099739c8a43
easonyen16 <eason990024@gmail.com>
2023-04-11 00:45:49 +0800

    Add files via upload

---
+++ 第一次期中考/7.c
@@ -0,0 +1,107 @@
+#include <stdio.h>
+
+int main(){
+    int t, i, digitStart, digitEnd, letterStart, letterEnd, digitSum, letterSum, absDifference;
+    char str[15] = {'\0'};
+    //scanf("%d", &t);
+    int cases=1;
+    while (scanf("%s", &str)!=EOF){
+        printf("Case #%d: ", cases);
+        if (str[0] >= '0' && str[0] <= '9'){
+            digitStart = 0;
+            if(str[2]=='-'){
+                letterStart = 3;
+                digitEnd = 1;
+                if(str[5]=='\0'){
+                    letterEnd = 4;
+                }else{
+                    letterEnd = 5;
+                }
+            }
+            else if(str[3]=='-'){
+                letterStart = 4;
+                digitEnd = 2;
+                if(str[6]=='\0'){
+                    letterEnd = 5;
+                }else{
+                    letterEnd = 6;
+                }
+            }
+            else if(str[4]=='-'){
+                letterStart = 5;
+                digitEnd = 3;
+                if(str[7]=='\0'){
+                    letterEnd = 6;
+                }else{
+                    letterEnd = 7;
+                }
+            }
+        }else{
+            letterStart = 0;
+            if(str[2]=='-'){
+                digitStart = 3;
+                letterEnd = 1;
+                if(str[5]=='\0'){
+                    digitEnd = 4;
+                }else if(str[6]=='\0'){
+                    digitEnd = 5;
+                }else{
+                    digitEnd = 6;
+                }
+            }else{
+                digitStart = 4;
+                letterEnd = 2;
+                if(str[6]=='\0'){
+                    digitEnd = 5;
+                }else if(str[7]=='\0'){
+                    digitEnd = 6;
+                }else{
+                    digitEnd = 7;
+                }
+            }
+        }
+        for (i = digitStart, digitSum = 0; i <= digitEnd; i++){
+            digitSum = digitSum * 10 + str[i] - '0';
+        }
+        for (i = letterStart, letterSum = 0; i <= letterEnd; i++){
+            letterSum = letterSum * 26 + str[i] - 'A';
+        }
+        if(digitStart==0){
+            printf("\"");
+            for (i = digitStart; i <= digitEnd; i++){
+                printf("%c",str[i]);
+            }
+            printf("\" is %d and \"",digitSum);
+            for (i = letterStart; i <= letterEnd; i++){
+                printf("%c",str[i]);
+            }
+            printf("\" is %d where ",letterSum);
+        }else{
+            printf("\"");
+            for (i = letterStart; i <= letterEnd; i++){
+                printf("%c",str[i]);
+            }
+            printf("\" is %d and \"",letterSum);
+            for (i = digitStart; i <= digitEnd; i++){
+                printf("%c",str[i]);
+            }
+            printf("\" is %d where ",digitSum);
+        }
+        absDifference = digitSum - letterSum;
+        if (absDifference < 0){
+            absDifference = -absDifference;
+        }
+        if ('0' <= str[0] && str[0] <= '9'){
+            printf("|%d-%d|=|%d|=%d ", digitSum, letterSum, digitSum - letterSum, absDifference);
+        }
+        else{
+            printf("|%d-%d|=|%d|=%d ", letterSum, digitSum, letterSum - digitSum, absDifference);
+        }
+        if (absDifference > 100){
+            printf("is not nice\n");
+        }else{
+            printf("is nice\n");
+        }
+        cases++;
+    }
+}
